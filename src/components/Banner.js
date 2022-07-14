1f9dae741e1c072effebbe7a6ea1bc80bb49bef1
James Anuncio <anunciojames14@gmail.com>
2022-07-14 18:00:06 +0800

    FEAT: Portfolio

---
+++ src/components/Banner.js
@@ -0,0 +1,20 @@
+
+
+function Banner() {
+  return (
+    <div>
+        <div className="flex justify-center items-center my-20">
+            <div className=" font-['Poppins_sans'] uppercase w-[420px]">
+                <p className="text-[8px] font-semibold tracking-wider">FULL STACK WEB DEVELOPER</p>
+                <h2 className="text-[50px] font-bold leading-snug  drop-shadow-[0_0_10px_rgba(130,246,229,0.6)]">Hey Everyone! Im James </h2>
+                <p className="text-[8px] tracking-wider">I create websites that help connect with everyone. COME TAKE A LOOK AT MY PORTOLIO AND SEE MY BEST WEB DESIGN SKILLS</p>
+                <button className="my-5 h-8 w-32 bg-[#11eccc] text-white">View Portfolio</button>
+            </div>
+            <div className="w-[280px]"></div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Banner;
---
+++ src/components/Header.js
@@ -0,0 +1,23 @@
+import Banner from "./Banner";
+
+
+function Header() {
+  return (
+    <div className="bg-[url('https://demo.dwtutorial.com/qreato/wp-content/uploads/sites/10/2021/11/section-4.jpeg')] h-screen bg-cover bg-[top_center">
+        <div className="flex justify-between px-44">
+            <h1 className="text-[25px] mt-2">Cloud</h1>
+
+            <ul className="flex gap-5 py-5 text-[10px]">
+                <li>Home</li>
+                <li>Career</li>
+                <li>About</li>
+                <li>Portfolio</li>
+            </ul>
+        </div>
+
+        <Banner />
+    </div>
+  )
+}
+
+export default Header
